5fc7ffc5b56d5847051270958d81d40cf3025580
adudak <adudak64a@gmail.com>
2023-03-24 19:46:55 +0200

    repo - small change

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, path::PathBuf};
+use std::env;
 use include_dir::{include_dir, Dir, File};
 use std::path::Path;
 
@@ -16,15 +16,13 @@ fn main() {
 
     if command == "show"{
         let var2 = show_command(&command2, &mut v);
-        println!("{}", var2.display());
-        println!("sdfs");
         let lib_rs =  PROJECT_DIR.get_file(var2).unwrap();
         let content = lib_rs.contents_utf8().unwrap();
         output(content);
     }
 }
 
-fn show_command<'a>(command_name: &str, file_name: &'a mut [&File]) -> &'a Path {
+fn show_command<'k>(command_name: &str, file_name: &'k mut [&File]) -> &'k Path {
     for entry in file_name {
         let path = Path::new(command_name);
         let new_path = path.with_extension("txt");
