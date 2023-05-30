da839f2046ed47b0318b42810221f061cd8a88c4
adudak <adudak64a@gmail.com>
2023-05-30 06:34:58 +0300

    added error handling, used match operator

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,47 @@
-use std::{env, fs};
-use include_dir::{include_dir, Dir, File};
-use std::path::{Path, PathBuf};
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    static PROJECT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/src/my_log");
-    
-    if &args[1] == "show"{
-        let var2 = make_path(&args).with_extension("txt");
-        let lib_rs =  PROJECT_DIR.get_file(var2).unwrap();
-        let content = lib_rs.contents_utf8().unwrap();
-        output(content);
-    }
-    if &args[1] == "-a" || &args[1] == "--about" {
-        about();
+use std::{env};
+use include_dir::{include_dir, Dir};
+use std::path::{PathBuf};
+mod about_info;
+
+struct ReadPathError{details: String}
+
+impl ReadPathError {
+    fn new(msg: &str) -> ReadPathError {
+        ReadPathError{details: msg.to_string()}
     }
 }
 
-fn make_path(path_vec: &Vec<String>) -> PathBuf {
+fn make_path<'k>(path_vec: &Vec<String>, project_dir: &Dir<'static>) -> Result<&'k str, ReadPathError> {
     let mut path_1 = PathBuf::new();
     for i in 2..path_vec.len(){
         path_1.push(&path_vec[i]);
     }
-    return path_1;
+    let var2 = path_1.with_extension("txt");
+    if project_dir.get_file(&var2).is_some() == false{
+        Err(ReadPathError::new("Looks like this note is not exist"))
+    } else{
+        let lib_rs =  project_dir.get_file(&var2).unwrap();
+        let content = lib_rs.contents_utf8().unwrap();
+        Ok(content)
+    }
+}
+fn main() {
+    env::set_var("RUST_BACKTRACE", "0");
+    let args: Vec<String> = env::args().collect();
+    static PROJECT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/src/my_log");
+
+    let first_par: &str= &args[1];
+
+    match first_par {
+        "print" => {
+            match make_path(&args, &PROJECT_DIR){
+                Ok(content) => output(content),
+                Err(error) => println!("{}", error.details)
+            };
+    },
+        "-a" | "--about" => about_info::about(),
+        _ => println!("Someething wrong with first parameter")
+    }
 }
 
 
@@ -31,34 +51,3 @@ fn output(output_info: &str){
     println!("\n=====================================");
 }
 
-fn about(){
-    println!(",,,,,,,,,,,,,,,,:+?%#@@@@@@@@#%?+:,,,,,,,,,,,,,,,,
-,,,,,,,,,,,,,:*S@@@@#%%????%%#@@@@S*:,,,,,,,,,,,,,
-,,,,,,,,,,,;%@@@S*;,,,,::::,,,,;*S@@@%;,,,,,,,,,,,      
-,,,,,,,,,:%@@#*:,,;*%#@@@@@@#S*;,,:*#@@%:,,,,,,,,,
-,,,,,,,,+#@@*:,:?#@@@S?****%S@@@#?:,:*@@#+,,,,,,,,
-,,,,,,,+@@#;,,*@@@%;,,,::::,,,;%@@@*,,;#@@+,,,,,,,
-,,,,,,;@@@;,,%@@%:,,+%#@@@@#%+,,:%@@%,,;@@@;,,,,,,
-,,,,,,%@@*,,?@@%,,;#@@S?++?S@@#;,,%@@?,,*@@%,,,,,,
-,,,,,:@@@:,:@@#:,:@@@+,,;;,,+@@@:,:#@@:,:@@@:,,,,,
-,,,,,;@@#,,;@@S,,+@@%,,*@@*,,%@@+,,S@@;,,#@@:,,,,,
-,,,,,:#S+,,%@@?,,?@@?,,%@@*,,%@@+,,S@@;,,#@@;,,,,,
-,,,,,,:,,;%@@S:,:@@@:,:#@@;,:#@@:,,S@@;,,#@@;,,,,,
-,,,,,:?%#@@#*,,;#@@*,,?@@%,,*@@%,,,S@@;,,#@@;,,,,,
-,,,,,:##S?+,,:?@@@+,,*@@#:,:#@@;,,,S@@;,,#@@;,,,,,
-,,,,,,,,,,:+%@@@%:,:?@@S:,:S@@*,,,,S@@;,,#@@;,,,,,
-,,,,,:%%S#@@@#?:,,+#@@%:,:S@@?,,,,,S@@+,,S@@:,,,,,
-,,,,,:##S%?+:,,:*#@@S+,,+#@@*,,+?,,*@@%,,;%S:,,,,,
-,,,,,,,,,,,:+?S@@@%+,,;%@@S;,,*@@;,,%@@S+:,,,,,,,,
-,,,,,,:%S#@@@@#%+:,,+S@@#*,,,,+@@#+,,+S@@@S:,,,,,,
-,,,,,,,+SS?*+:,,:+?#@@#*:,:?*,,;#@@%;,,;*%+,,,,,,,
-,,,,,,,,,,::;+?S@@@#?;,,;?@@@%:,:?#@@S*;,,,,,,,,,,
-,,,,,,,,,:?@@@@#%*;,,:+S@@@%@@#*:,:*S@@%:,,,,,,,,,
-,,,,,,,,,,,;+;:,,:;*S@@@S*:,;%@@@?;:,:;,,,,,,,,,,,
-,,,,,,,,,,,,,,,+%#@@@S*:,,;+,,;?#@S+,,,,,,,,,,,,,,
-,,,,,,,,,,,,,,,::+*+:,,:*S@@%;,,::,,,,,,,,,,,,,,,,\n
-                    Imprint
--------------------------------------------------
-This is a program for creating your own structure
-of notes and using them in the console!");
-}
